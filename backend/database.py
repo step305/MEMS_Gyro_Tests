edacf1ae5b5d8b662b79969be0000cfdaf9a0c71
step305 <step305@mail.ru>
2022-06-21 10:18:56 +0300

    0.87 fixes

--- backend/database.py
+++ backend/database.py
@@ -14,27 +14,83 @@ class SensorsBase:
             self.cursor.execute(sql_req)
             record = self.cursor.fetchall()
             print(record)
+            sql_req = 'CREATE TABLE IF NOT EXISTS tests (id INTEGER PRIMARY KEY DEFAULT 0);'
+            self.cursor.execute(sql_req)
+            self.base_connection.commit()
             sql_req = 'CREATE TABLE IF NOT EXISTS sensors (name TEXT PRIMARY KEY);'
             self.cursor.execute(sql_req)
             self.base_connection.commit()
             sql_req = 'CREATE TABLE IF NOT EXISTS bandwidth_result (id INTEGER PRIMARY KEY,' \
-                      'name TEXT NOT NULL,' \
+                      'name TEXT NOT NULL, ' \
+                      'sensor_id TEXT, ' \
+                      'test_id INTEGER NOT NULL,' \
                       'date TIMESTAMP NOT NULL,' \
                       'bandwidth REAL NOT NULL,' \
                       'temperature REAL NOT NULL,' \
                       'rate_max REAL NOT NULL,' \
+                      'test_type TEXT,' \
+                      'comment TEXT,' \
                       'data BLOB);'
             self.cursor.execute(sql_req)
             self.base_connection.commit()
             sql_req = 'CREATE TABLE IF NOT EXISTS static_results (id INTEGER PRIMARY KEY,' \
                       'name TEXT NOT NULL,' \
+                      'sensor_id TEXT,' \
+                      'test_id INTEGER NOT NULL,' \
                       'date TIMESTAMP NOT NULL,' \
                       'scale REAL NOT NULL,' \
                       'bias REAL NOT NULL,' \
                       'nonlin REAL NOT NULL,' \
                       'temperature REAL NOT NULL,' \
                       'rate_max REAL NOT NULL,' \
+                      'test_type TEXT,' \
+                      'comment TEXT,' \
                       'data BLOB);'
+            self.cursor.execute(sql_req)
+            self.base_connection.commit()
+            sql_req = 'CREATE TABLE IF NOT EXISTS thermal_results (id INTEGER PRIMARY KEY,' \
+                      'name TEXT NOT NULL,' \
+                      'sensor_id TEXT,' \
+                      'test_id INTEGER NOT NULL,' \
+                      'date TIMESTAMP NOT NULL,' \
+                      'scale REAL NOT NULL,' \
+                      'bias REAL NOT NULL,' \
+                      'nonlin REAL NOT NULL,' \
+                      'temperature REAL NOT NULL,' \
+                      'rate_max REAL NOT NULL,' \
+                      'test_type TEXT,' \
+                      'comment TEXT,' \
+                      'data BLOB);'
+            self.cursor.execute(sql_req)
+            self.base_connection.commit()
+            sql_req = 'CREATE TABLE IF NOT EXISTS stability_results (id INTEGER PRIMARY KEY,' \
+                      'name TEXT NOT NULL,' \
+                      'sensor_id TEXT,' \
+                      'test_id INTEGER NOT NULL,' \
+                      'date TIMESTAMP NOT NULL,' \
+                      'arw REAL NOT NULL,' \
+                      'bias_instabiluty REAL NOT NULL,' \
+                      'temperature REAL NOT NULL,' \
+                      'test_type TEXT,' \
+                      'comment TEXT,' \
+                      'data BLOB);'
+            sql_req = 'CREATE TABLE IF NOT EXISTS sensor_datasheet (id INTEGER PRIMARY KEY,' \
+                      'name TEXT NOT NULL,' \
+                      'sensor_id TEXT,' \
+                      'date TIMESTAMP NOT NULL,' \
+                      'scale REAL,' \
+                      'bias REAL,' \
+                      'nonlin REAL,' \
+                      'max_scale_in_temperature REAL,' \
+                      'max_bias_in_temperature REAL' \
+                      'arw REAL,' \
+                      'bias_instability REAL,' \
+                      'static_results_id INTEGER,' \
+                      'thermal_results_id INTEGER,' \
+                      'stability_results_id INTEGER,' \
+                      'comment TEXT);'
+            self.cursor.execute(sql_req)
+            self.base_connection.commit()
             self.cursor.execute(sql_req)
             self.base_connection.commit()
             self.cursor.close()
@@ -42,28 +98,31 @@ class SensorsBase:
         except sqlite3.Error as error:
             print('Error during connection to database:', error)
 
-    def add_static_result(self, sensor, temperature, rate_max):
+    def add_static_result(self, test_id, sensor, temperature, rate_max):
         self.cursor = self.base_connection.cursor()
         sql_req = 'INSERT OR IGNORE INTO sensors (name) VALUES (?);'
         self.cursor.execute(sql_req, (sensor.name,))
-        sql_req = 'INSERT INTO static_results (name,date, scale, ' \
-                  'bias, nonlin, rate_max, data, temperature) ' \
-                  'VALUES (?, ?, ?, ?, ?, ?, ?, ?);'
-        self.cursor.execute(sql_req, (sensor.name, datetime.datetime.now(),
+        sql_req = 'INSERT INTO static_results (name, sensor_id, test_id, date, scale, ' \
+                  'bias, nonlin, rate_max, data, temperature, test_type, comment) ' \
+                  'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);'
+        self.cursor.execute(sql_req, (sensor.name, sensor.id, test_id, datetime.datetime.now(),
                                       sensor.scale, sensor.bias, sensor.nonlin, rate_max,
-                                      pickle.dumps(sensor.history), temperature))
+                                      pickle.dumps(sensor.history), temperature,
+                                      sensor.test_type, sensor.comment))
         self.base_connection.commit()
         self.cursor.close()
 
-    def add_bandwidth_result(self, sensor, temperature, rate):
+    def add_bandwidth_result(self, test_id, sensor, temperature, rate):
         self.cursor = self.base_connection.cursor()
         sql_req = 'INSERT OR IGNORE INTO sensors (name) VALUES (?);'
         self.cursor.execute(sql_req, (sensor.name,))
-        sql_req = 'INSERT INTO bandwidth_result (name, date, bandwidth, rate_max, data, temperature) ' \
-                  'VALUES (?, ?, ?, ?, ?, ?);'
-        self.cursor.execute(sql_req, (sensor.name, datetime.datetime.now(),
+        sql_req = 'INSERT INTO bandwidth_result (name, test_id, date, ' \
+                  'bandwidth, rate_max, data, temperature, test_type, comment) ' \
+                  'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);'
+        self.cursor.execute(sql_req, (sensor.name, test_id, datetime.datetime.now(),
                                       sensor.bandwidth, rate,
-                                      pickle.dumps(sensor.out), temperature))
+                                      pickle.dumps(sensor.out), temperature,
+                                      sensor.test_type, sensor.comment))
         self.base_connection.commit()
         self.cursor.close()
 
--- config.py
+++ config.py
@@ -2,10 +2,26 @@ import numpy as np
 import os
 
 SENSORS_DATABASE = os.path.abspath(os.path.join(os.path.curdir, 'database', 'database.db'))
+TEST_TYPES = (
+    'первичный',
+    'после вибрации',
+    'после удара',
+    'после перегрузки',
+    'после термоцикла',
+    'финальный'
+)
+TEST_TYPE = TEST_TYPES[0]
 
 
 class RateSensor:
-    def __init__(self, name='', out=None, ref=None, temper=None, plt_color='b-.'):
+    def __init__(self,
+                 name='',
+                 sensor_id='',
+                 out=None,
+                 ref=None,
+                 temper=None,
+                 comment='',
+                 plt_color='b-.'):
         self.out_chan = out
         self.temp_chan = temper
         self.ref_chan = ref
@@ -17,9 +33,12 @@ class RateSensor:
         self.bias = 0
         self.nonlin = 0
         self.name = name
+        self.id = sensor_id
         self.plt_color_fmt = plt_color
         self.out = []
         self.bandwidth = 0
+        self.test_type = TEST_TYPE
+        self.comment = comment
 
     def extract(self, adc_result):
         self.out = adc_result[self.out_chan]
@@ -33,6 +52,17 @@ class RateSensor:
     def add(self, rate, out, stdout):
         self.history.append((rate, out, stdout))
 
+    def reset_results(self):
+        self.history = []
+        self.mean_value = 0
+        self.std_value = 0
+        self.temper_value = 0
+        self.bandwidth = 0
+        self.scale = 0
+        self.bias = 9
+        self.nonlin = 0
+        self.out = []
+
 
 # PSU configuration
 PSU_PORT = 'COM28'
@@ -45,8 +75,8 @@ RATE_TABLE_PORT = 'COM7'
 RATE_TABLE_BAUD_RATE = 38400
 
 # Thermo (climatic) camera configuration
-THERMO_PORT = 'COM10'
-THERMO_BAUDRATE = 19200
+THERMO_PORT = 'COM15'
+THERMO_BAUDRATE = 9600
 
 # NIDAQ configuration
 ADC_CHANNELS = [0, 8, 1, 9, 3, 11, 5, 4, 12, 13, 6, 14, 7, 15, 16, 24, 18, 17, 25, 26, 20]
@@ -54,20 +84,20 @@ NIDAQ_ID = 'Dev5'
 ADC_RATE = 1000
 ACQUISITION_TIME = 10
 Sensors = [
-    RateSensor(name='VG910_1', out=0, temper=1, plt_color='b-.'),
-    RateSensor(name='VG910_2', out=2, temper=3, plt_color='r-.'),
-    RateSensor(name='TG100_1', out=4, ref=5, temper=6, plt_color='g-.'),
-    RateSensor(name='TG100_2', out=7, ref=8, temper=9, plt_color='y-.'),
-    RateSensor(name='CRH02', out=10, ref=11, temper=12, plt_color='b*'),
-    RateSensor(name='CRS03', out=13, plt_color='k*'),
-    RateSensor(name='TG19A', out=14, ref=15, temper=16, plt_color='m*'),
-    RateSensor(name='CRS09', out=17, ref=18, temper=19, plt_color='c*'),
-    RateSensor(name='5V', out=20, plt_color='g*')
+    RateSensor(name='VG910', sensor_id='1', out=0, temper=1, comment='Fizoptika', plt_color='b-.'),
+    RateSensor(name='VG910', sensor_id='2', out=2, temper=3, comment='Fizoptika', plt_color='r-.'),
+    RateSensor(name='TG100', sensor_id='1', out=4, ref=5, temper=6, comment='MPLab', plt_color='g-.'),
+    RateSensor(name='TG100', sensor_id='2', out=7, ref=8, temper=9, comment='MPLab', plt_color='y-.'),
+    RateSensor(name='CRH02', sensor_id='0', out=10, ref=11, temper=12, comment='SiliconSensing', plt_color='b*'),
+    RateSensor(name='CRS03', sensor_id='0', out=13, comment='SiliconSensing', plt_color='k*'),
+    RateSensor(name='TG19A', sensor_id='0', out=14, ref=15, temper=16, comment='SiliconSensing', plt_color='m*'),
+    RateSensor(name='CRS09', sensor_id='0', out=17, ref=18, temper=19, comment='MPLab', plt_color='c*'),
+    RateSensor(name='5V', sensor_id='-1', out=20, comment='power rail', plt_color='g*')
 ]
 
 # static test plan
-MAX_RATE = [50, 100, 200, 400]
-RATE_STEP = [10, 20, 40, 80]
+MAX_RATE = [100, 75]
+RATE_STEP = [20, 15]
 
 # bandwidth test plan
 rate_bandw = 50
